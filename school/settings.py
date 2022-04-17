0f1747663811726ffa0af1c9c41e386c459409e4
che30 <che.blancoo58@gmail.com>
2022-04-17 15:12:21 +0100

    Update settings.py

--- school/settings.py
+++ school/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost']
+ALLOWED_HOSTS = ['manageschooldjango.herokuapp.com','localhost']
 
 AUTH_USER_MODEL = 'account.Account'
 # Application definition
